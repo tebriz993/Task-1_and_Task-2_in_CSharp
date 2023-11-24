4039df86260351fef765bd15032e8fa66b9d754d
Tabriz Latifov <130743534+tebriz993@users.noreply.github.com>
2023-11-24 13:40:11 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,93 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task2_CSharp
+namespace Homework1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            MasterCard mastercard1 = new MasterCard(1000,1234);
-            mastercard1.forMasterCardCheckBalance();
 
-            VisaCard visaCard1 = new VisaCard(1000,1232);
-            visaCard1.forVisaCardCheckBalance();
+            #region Homework2
+            string a = "the sky is blue";
+            string[] words = a.Split(' ');
 
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+
+                if (!string.IsNullOrWhiteSpace(words[i]))
+                {
+                    Console.Write(words[i]);
+
+                    if (i > 0)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            #endregion
         }
 
     }
 
-     interface IBankCard 
-     {
-        void forVisaCardPaying();
-        void forVisaCardCheckBalance();
 
-
-        void forMasterCardPaying();
-        void forMasterCardCheckBalance();
-
-     }
-
-
-
-    class VisaCard
+    #region Homework1
+    public class Solution
     {
-        public readonly int amount = 1000;
-        public int forVisaCardPaying_Amount;
-        public int End_Amount;
-
-        public VisaCard(int amount, int forVisaCardPaying_Amount)
+        public int LengthOfLastWord(string s)
         {
-            this.amount = amount;
-            this.forVisaCardPaying_Amount = forVisaCardPaying_Amount;
 
+            var output = 0;
+            var determineWord = false;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    determineWord = true;
+                    output++;
+                    continue;
+                }
+                else if (determineWord)
+                {
+                    break;
+                }
+            }
+            return output;
 
         }
-
-        public void forVisaCardPaying()
-        {
-            Console.WriteLine($"Visacard ucun Odenilen mebleg: {forVisaCardPaying_Amount}");
-        }
-
-        public void forVisaCardCheckBalance()
-        {
-            Console.WriteLine($"Visacard ucun Son mebleg: " + (amount + forVisaCardPaying_Amount));
-        }
-
     }
-
-
-
-    class MasterCard
-    {
-        public readonly int amount = 1000;
-        public int forMasterCardPaying_Amount;
-        public int End_Amount;
-
-        public MasterCard(int amount, int forMasterCardPaying_Amount)
-        {
-            this.amount = amount;
-            this.forMasterCardPaying_Amount = forMasterCardPaying_Amount;
-
-
-        }
-
-        public void forMasterCardPaying()
-        {
-            Console.WriteLine($"Mastercard ucun Odenilen mebleg: {forMasterCardPaying_Amount}");
-        }
-
-        public void forMasterCardCheckBalance()
-        {
-            Console.WriteLine($"Mastercard ucun Son mebleg: " + (amount + forMasterCardPaying_Amount));
-        }
-
-    }
-
-
-
-
-
-
+    #endregion
 
 }
